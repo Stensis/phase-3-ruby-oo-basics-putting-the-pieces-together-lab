1a7b634382061cc1b8a10c812052f5588e202800
Stensis <irenenjuguna98@gmail.com>
2022-08-16 23:04:42 +0300

    commit tree 445388a3b7a69613f60451099e4aa157cafa177c

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,52 @@
+class Book
+    def initialize(title)
+         @title = title
+     end
 
+    def title=(new_title)
+         @title = new_title
+     end
 
+     def author=(new_author)
+        @author = new_author
+    end
+
+    def page_count=(new_page_count)
+       @page_count = new_page_count
+     end
+
+     def genre=(new_genre)
+         @genre = new_genre
+     end
+
+    def title
+         @title 
+     end
+
+     def author
+         @author 
+    end
+
+     def page_count
+         @page_count 
+     end
+
+     def genre
+        @genre 
+     end
+
+     def turn_page
+         puts "Flipping the page...wow, you read fast!"
+     end
+ end
+
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize(title)
+        @title = title
+    end 
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
